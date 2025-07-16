3973113b0f3307e2112965bb8940b5b2e579b3f9
Peter Overeijnder <peterovereijnder@hotmail.com>
2025-07-16 13:36:31 +0200

    -

--- Bedford Falls V3/media/maps/BedfordFalls/spawnregions.lua
+++
@@ -1,7 +0,0 @@
-function SpawnRegions()
-	return {
-		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
-		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
-		{ name = "Bedford Falls", file = "media/maps/BedfordFalls/spawnpoints.lua" },
-	}
-end
