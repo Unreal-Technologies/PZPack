1b951fc8af47abc3500fc8635c4025871a52bf5f
Peter Overeijnder <peterovereijnder@hotmail.com>
2025-07-16 13:10:15 +0200

    -

--- Taylorsville/media/lua/server/items/TaylorsvilleMapDistribution.lua
+++
@@ -1,5 +0,0 @@
-local function preDistributionMerge()
-    table.insert(ProceduralDistributions.list.MagazineRackMaps.items, "TaylorsvilleMap");
-    table.insert(ProceduralDistributions.list.MagazineRackMaps.items, 50);
-end
-Events.OnPreDistributionMerge.Add(preDistributionMerge);
